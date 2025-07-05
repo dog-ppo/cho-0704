a8dedbae187adf2271cbb1783d37e94579effa7b
dog-ppo <154861106+dog-ppo@users.noreply.github.com>
2025-07-04 18:53:41 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -8,8 +8,7 @@ const NEZHA_PORT = process.env.NEZHA_PORT || '443';                     // 哪�
 const NEZHA_KEY = process.env.NEZHA_KEY || 'J98JaH0S4R5pXNHWon';   // 没用哪吒，把这个参数空着
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'choreo00.dyhdg.tk
 ';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWU2ZTI0MWExM2RkNjIzZDQ5YzIyMTJiNmQ0YjZjYWMiLCJ0IjoiMzk2YTQ2NjctZWVkNy00ZGZhLWFiZmEtMDg1ODcxODk5NTk0IiwicyI6Ik16QXlPR0UyWXpNdE16QTNPQzAwT0dOaUxXSmlaR1V0TkRNMU5EQXlOek5rTm1FdyJ9
-';
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZWU2ZTI0MWExM2RkNjIzZDQ5YzIyMTJiNmQ0YjZjYWMiLCJ0IjoiMzk2YTQ2NjctZWVkNy00ZGZhLWFiZmEtMDg1ODcxODk5NTk0IiwicyI6Ik16QXlPR0UyWXpNdE16QTNPQzAwT0dOaUxXSmlaR1V0TkRNMU5EQXlOek5rTm1FdyJ9';
 const CFIP = process.env.CFIP || 'www.visa.com.tw';
 const NAME = process.env.NAME || 'Choreo';
 
